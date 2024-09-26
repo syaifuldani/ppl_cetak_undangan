fc7cbd61f4692e7e51d42a1bd95160822cfd6535
Vems04-sync <138218233+Vems04-sync@users.noreply.github.com>
2024-09-26 10:58:25 +0700

    update dashboard

--- customer/dashboard.php
+++ customer/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php"); // Jika belum login, redirect ke halaman login
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
