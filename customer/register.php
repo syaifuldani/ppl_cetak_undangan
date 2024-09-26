33c654251394b31c29fff687fdc5025e060b318b
Vems04-sync <138218233+Vems04-sync@users.noreply.github.com>
2024-09-26 10:24:17 +0700

    Update Halaman Home Customer

--- customer/register.php
+++ customer/register.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Postcard Register Form</title>
     <link rel="stylesheet" href="../resources/css/loginregis.css">
 </head>
+
 <body>
     <div class="container">
         <div class="image-section">
@@ -14,19 +16,23 @@
         <div class="form-section">
             <h2>Register</h2>
             <form action="#" method="POST">
+                <label for="nama_lengkap">Nama Lengkap</label>
+                <input type="text" id="nama_lengkap" name="nama_lengkap" required>
+
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" required>
-                
+
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password" required>
-                
+
                 <label for="confirm-password">Repeat Password</label>
                 <input type="password" id="confirm-password" name="confirm-password" required>
-                
+
                 <button type="submit">Register</button>
             </form>
             <p>Sudah punya akun? <a href="login.php">Login sini</a></p>
         </div>
     </div>
 </body>
+
 </html>
