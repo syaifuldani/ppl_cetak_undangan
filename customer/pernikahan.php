e24db5e2713c1b474e501037167c1f8648e2d980
Vems04-sync <138218233+Vems04-sync@users.noreply.github.com>
2024-09-27 10:54:49 +0700

    test

--- customer/pernikahan.php
+++ customer/pernikahan.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: index.php"); // Jika belum login, redirect ke halaman login
+  exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
