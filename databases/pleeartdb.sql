e32cb9cc2b227a23165b4c0d47e06e3a926f076e
Syaiful Rochmandani <syaifulrochmandani737@gmail.com>
2024-09-29 21:19:00 +0700

    database new

--- databases/pleeartdb.sql
+++ databases/pleeartdb.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2024 at 06:37 AM
+-- Generation Time: Sep 29, 2024 at 04:17 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,11 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `carts` (
-  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cart_id` int(11) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `jumlah` int(11) NOT NULL,
-  PRIMARY KEY (`cart_id`)
+  `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -41,11 +41,10 @@ CREATE TABLE `carts` (
 --
 
 CREATE TABLE `custom_design` (
-  `custom_design_id` int(11) NOT NULL AUTO_INCREMENT,
+  `custom_design_id` int(11) NOT NULL,
   `order_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `template_design` blob DEFAULT NULL,
-  PRIMARY KEY (`custom_design_id`)
+  `template_design` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -55,14 +54,13 @@ CREATE TABLE `custom_design` (
 --
 
 CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `tanggal_pemesanan` timestamp NOT NULL DEFAULT current_timestamp(),
   `status_pemesanan` enum('perludibayar','terkirim','dikemas','selesai','dibatalkan','perludikirim') DEFAULT 'perludibayar',
   `total_harga` decimal(10,2) NOT NULL,
   `metode_pembayaran` enum('transfer bank','e-wallet','lainnya') DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`order_id`)
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -72,13 +70,12 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `order_items` (
-  `detail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `detail_id` int(11) NOT NULL,
   `order_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `jumlah_order` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `harga_order_item` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`detail_id`)
+  `harga_order_item` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -88,13 +85,12 @@ CREATE TABLE `order_items` (
 --
 
 CREATE TABLE `payments` (
-  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `payment_id` int(11) NOT NULL,
   `order_id` int(11) DEFAULT NULL,
   `payment_status` enum('pending','success','failed') DEFAULT 'pending',
   `payment_type` varchar(50) DEFAULT NULL,
   `gross_amount` decimal(10,2) NOT NULL,
-  `transaction_time` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`payment_id`)
+  `transaction_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -104,7 +100,7 @@ CREATE TABLE `payments` (
 --
 
 CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
   `nama_produk` varchar(100) NOT NULL,
   `deskripsi` text DEFAULT NULL,
   `harga_product` decimal(10,2) NOT NULL,
@@ -112,8 +108,7 @@ CREATE TABLE `products` (
   `gambar_satu` longblob DEFAULT NULL,
   `gambar_dua` longblob DEFAULT NULL,
   `gambar_tiga` longblob DEFAULT NULL,
-  `kategori` varchar(50) NOT NULL,
-  PRIMARY KEY (`product_id`)
+  `kategori` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -123,14 +118,13 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `shipments` (
-  `shipment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `shipment_id` int(11) NOT NULL,
   `order_id` int(11) DEFAULT NULL,
   `ekspedisi` varchar(50) NOT NULL,
   `nomor_resi` varchar(50) DEFAULT NULL,
   `status_pengiriman` enum('dalam perjalanan','diterima') DEFAULT NULL,
   `tanggal_pengiriman` date DEFAULT NULL,
-  `alamat_pengiriman` text NOT NULL,
-  PRIMARY KEY (`shipment_id`)
+  `alamat_pengiriman` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -140,46 +134,181 @@ CREATE TABLE `shipments` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `nama_lengkap` varchar(100) NOT NULL,
+  `profile_image` blob NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(256) NOT NULL,
   `alamat` text NOT NULL,
   `nomor_telepon` varchar(20) DEFAULT NULL,
   `jenis_pengguna` enum('admin','customer') NOT NULL DEFAULT 'customer',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`user_id`)
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `users`
+--
 
+INSERT INTO `users` (`user_id`, `nama_lengkap`, `profile_image`, `email`, `password`, `alamat`, `nomor_telepon`, `jenis_pengguna`, `created_at`) VALUES
+(1, 'Ko Yunus', 0x2e2e2f637573746f6d65722f75706c6f6164732f313732373539313930375f47616d62617220576861747341707020323032342d30392d31372070756b756c2032332e34322e31325f38303038633465632e6a7067, 'test@gmail.com', '$2y$10$empyyawUTw1n9.pMMj5Z6eON4Z7Ee7PKyGzeoRketNPVB1PKDOZaS', 'Jalan Bangakalan Telnag', '', 'customer', '2024-09-28 13:03:01'),
+(16, 'ripkunadmin', '', 'ripkun@gmail.com', '$2y$10$LZHgaV2bs7fOKmWcg2kNj.Am1uYqM9SOLZH2EZMCecxr8HaV94SE.', '', NULL, 'admin', '2024-09-29 12:51:40'),
+(17, 'andre sujatmiko', 0x2e2e2f637573746f6d65722f75706c6f6164732f313732373631353032395f706572636f6261616e2e6a7067, 'andre@gmail.com', '$2y$10$73pbXeGA2SXzRafztAbKSuxAGXwz/1glAhm50sTjsQ9VhZ9hOfbQS', '', '', 'customer', '2024-09-29 12:59:03');
+
+--
+-- Indexes for dumped tables
+--
+
+--
 -- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `FK_carts_ibfk_1` (`product_id`),
+  ADD KEY `carts_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `custom_design`
+--
+ALTER TABLE `custom_design`
+  ADD PRIMARY KEY (`custom_design_id`),
+  ADD KEY `FK_custom_design_ibfk_1` (`user_id`),
+  ADD KEY `FK_custom_design_ibfk_2` (`order_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `orders_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `FK_order_items_ibfk_2` (`product_id`),
+  ADD KEY `order_items_ibfk_1` (`order_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `payments_ibfk_1` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `shipments`
+--
+ALTER TABLE `shipments`
+  ADD PRIMARY KEY (`shipment_id`),
+  ADD KEY `shipments_ibfk_1` (`order_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `custom_design`
+--
+ALTER TABLE `custom_design`
+  MODIFY `custom_design_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shipments`
+--
+ALTER TABLE `shipments`
+  MODIFY `shipment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `carts`
+--
 ALTER TABLE `carts`
   ADD CONSTRAINT `FK_carts_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
   ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
--- Indexes for table `custom_design`
+--
+-- Constraints for table `custom_design`
+--
 ALTER TABLE `custom_design`
   ADD CONSTRAINT `FK_custom_design_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `FK_custom_design_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 
--- Indexes for table `orders`
+--
+-- Constraints for table `orders`
+--
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
--- Indexes for table `order_items`
+--
+-- Constraints for table `order_items`
+--
 ALTER TABLE `order_items`
   ADD CONSTRAINT `FK_order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
   ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 
--- Indexes for table `payments`
+--
+-- Constraints for table `payments`
+--
 ALTER TABLE `payments`
   ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 
--- Indexes for table `shipments`
+--
+-- Constraints for table `shipments`
+--
 ALTER TABLE `shipments`
   ADD CONSTRAINT `shipments_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
